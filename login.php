8e999b81c68746816dfacfa77e56acf106b6890c
sanyamjindal <101120349+sanyamjindal@users.noreply.github.com>
2023-10-25 00:40:00 +0530

    !

--- login.php
+++ login.php
@@ -35,3 +35,33 @@ if (isset($_POST['login'])) {
     $conn->close();
 }
 ?>
+
+<!-- /////////      NOT SECURE /////////
+<?php
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "your_database_name";
+
+$conn = new mysqli($host, $user, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if (isset($_POST['login'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        echo "Login successful!";
+    } else {
+        echo "Login failed.";
+    }
+}
+
+$conn->close();
+?>
